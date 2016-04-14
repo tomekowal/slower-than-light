1f79f477b9193c25b4689cd607813df61fe2bf61
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2016-04-14 23:29:54 +0200

    Refactor Game to its own module closes #1

---
+++ Game.elm
@@ -0,0 +1,34 @@
+module Game (init, update, view, Action(..)) where
+
+import Html exposing (Html, text, div, button)
+import Html.Events exposing (onClick)
+import Effects exposing (Effects, Never)
+import Drill
+
+type alias Model =
+  { drill : Drill.Model }
+
+init : Int -> Int -> (Model, Effects Action)
+init metal counter =
+  ({ drill = Drill.init metal counter }, Effects.none)
+
+type Action
+  = Tick
+  | Drill Drill.Action
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+    case action of
+      Tick ->
+        ({ model |
+             drill = Drill.update Drill.Tick model.drill
+         }, Effects.none)
+      Drill act ->
+        ({ model |
+             drill = Drill.update act model.drill
+         }, Effects.none)
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  Drill.view (Signal.forwardTo address Drill) model.drill
+
--- Main.elm
+++ Main.elm
@@ -3,12 +3,12 @@ import Html.Events exposing (onClick)
 import StartApp
 import Time exposing (..)
 import Signal exposing (Signal, map)
-import Effects exposing (Effects, Never)
-import Drill
+import Effects exposing (Effects)
+import Game exposing (init, view, update, Action)
 
 ticks : Signal Action
 ticks =
-  Signal.map (\(timeStamp, tick) -> Tick) (timestamp (every second))
+  Signal.map (\(timeStamp, tick) -> Game.Tick) (timestamp (every second))
 
 app =
   StartApp.start { init = init 0 0
@@ -18,30 +18,3 @@ app =
 
 main =
   app.html
-
-type alias Model =
-  { drill : Drill.Model }
-
-init : Int -> Int -> (Model, Effects Action)
-init metal counter =
-  ( { drill = Drill.init metal counter }, Effects.none )
-
-type Action
-  = Tick
-  | Drill Drill.Action
-
-update : Action -> Model -> (Model, Effects Action)
-update action model =
-    case action of
-      Tick ->
-        ({ model |
-            drill = Drill.update Drill.Tick model.drill
-        }, Effects.none)
-      Drill act ->
-        ({ model |
-            drill = Drill.update act model.drill
-        }, Effects.none)
-
-view : Signal.Address Action -> Model -> Html
-view address model =
-  Drill.view (Signal.forwardTo address Drill) model.drill
