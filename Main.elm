ecb1f93a0fabfd41c4c69395fc43a0ac0258a40c
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2016-04-16 22:21:02 +0200

    Make tick every tenth of a second closes #4

--- Main.elm
+++ Main.elm
@@ -8,10 +8,10 @@ import Game exposing (init, view, update, Action)
 
 ticks : Signal Action
 ticks =
-  Signal.map (\(timeStamp, tick) -> Game.Tick) (timestamp (every second))
+  Signal.map (\(timeStamp, tick) -> Game.Tick) (timestamp (every (second/10)))
 
 app =
-  StartApp.start { init = init 0 2
+  StartApp.start { init = init 0 20
                  , view = view
                  , update = update
                  , inputs = [ticks] }
