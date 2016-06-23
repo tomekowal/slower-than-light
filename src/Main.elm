e234c742cc4ca87960555ccba611d5260d3b2cf3
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2016-06-23 08:48:08 +0200

    Refactor according to emacs

--- src/Main.elm
+++ src/Main.elm
@@ -12,49 +12,53 @@ main =
               , subscriptions = subscriptions }
 
 type alias Model =
-  { drill_button : CounterButton.Model
-  , drill_max_work_time : Int
-  , drill_current_work_time : Maybe Int
-  , metal : Int
-  }
+    { drill_button : CounterButton.Model
+    , drill_max_work_time : Int
+    , drill_current_work_time : Maybe Int
+    , metal : Int
+    }
 
 init : Int -> Int -> (Model, Cmd Msg)
 init metal work_time =
-  ({ drill_button = CounterButton.init "Deploy the drill"
-   , drill_current_work_time = Nothing
-   , drill_max_work_time = work_time
-   , metal = metal
-   }, Cmd.none)
+    ({ drill_button = CounterButton.init "Deploy the drill"
+     , drill_current_work_time = Nothing
+     , drill_max_work_time = work_time
+     , metal = metal
+     }, Cmd.none)
 
 type Msg
-  = Tick Time
-  | CounterButton CounterButton.Msg
+    = Tick Time
+    | CounterButton CounterButton.Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
     case action of
-      Tick time ->
-          case model.drill_current_work_time of
-              Nothing -> ( model, Cmd.none )
-              Just counter ->
-                  case counter == model.drill_max_work_time of
-                      True ->
-                          ({ model | drill_current_work_time = Nothing, metal = model.metal + 1 }
-                          , Cmd.none)
-                      False ->
-                          ({ model | drill_current_work_time = Just (counter + 1) }
-                          , Cmd.none)
-      CounterButton CounterButton.Click ->
-        ({ model |
-             drill_current_work_time = Just 0
-         }, Cmd.none)
+        Tick time ->
+            case model.drill_current_work_time of
+                Nothing -> ( model, Cmd.none )
+                Just counter ->
+                    case counter == model.drill_max_work_time of
+                        True ->
+                            ({ model | drill_current_work_time = Nothing, metal = model.metal + 1 }
+                            , Cmd.none)
+                        False ->
+                            ({ model | drill_current_work_time = Just (counter + 1) }
+                            , Cmd.none)
+        CounterButton CounterButton.Click ->
+            ({ model |
+                   drill_current_work_time = Just 0
+             }, Cmd.none)
 
 view : Model -> Html Msg
 view model =
     div []
         [map CounterButton (CounterButton.view model.drill_button model.drill_current_work_time model.drill_max_work_time)
-        , div [] [text("metal: " ++ (toString model.metal))]]
+        , label "metal" model]
+
+label : String -> Model-> Html Msg
+label caption model =
+    div [] [text(caption ++ ": " ++ (toString model.metal))]
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every (20 * millisecond) Tick
+    Time.every (20 * millisecond) Tick
